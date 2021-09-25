75f0b266b0000ab82475f43de52fa44cda4ed3b9
Melon-alie <rubel300ali@gmail.comm>
2021-09-26 02:20:12 +0600

    new added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Blood And Covid Vaccine Donation Team Agency</h2>
+            <p>Donating blood good for the doner.The last wave has made everyone – in rural and urban areas both – take COVID-19 more seriously.</p>
+            <h2>Total Doner: 1000 People</h2>
+            <h2>Total Doner: 1000 People</h2>
+        </div>
+    );
+};
+
+export default Header;
