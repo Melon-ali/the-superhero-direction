293692bc79ea5f75c41c4915566443cc241cdff8
Melon-alie <rubel300ali@gmail.comm>
2021-09-26 08:45:19 +0600

    header added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
+        <div className="bg-dark text-white text-center p-2">
             <h2>Blood And Covid Vaccine Donation Team Agency</h2>
             <p>Donating blood good for the doner.The last wave has made everyone – in rural and urban areas both – take COVID-19 more seriously.</p>
-            <h2>Total Doner: 1000 People</h2>
             <h2>Total Doner: 1000 People</h2>
         </div>
     );
